cd0ad437c744fe57b576e0378d3087c0274f0728
Seth Taylor <seth.taylor@zapproved.com>
2020-07-22 15:37:59 -0700

    Added an API client with methods for all the endpoints and refactored controller integration tests to use this client.

---
+++ Trace.API/Trace.API.Client/Handlers/IHttpHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Trace.Models;
+
+namespace Trace.API.Client.Handlers
+{
+    public interface IHttpHandler
+    {
+        Task<HttpResponseMessage> SendRequestWithBodyAsync(object content, HttpMethod method, string endpoint, TokenResponse token = null);
+    }
+}
---
+++ Trace.API/Trace.API.Client/ITraceApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Trace.Models;
+
+namespace Trace.API.Client
+{
+    public interface ITraceApiClient
+    {
+        Task<HttpResponseMessage> CreatAccountWithRetryAsync(AwsCognitoUser user);
+        Task<HttpResponseMessage> ConfirmAccountWithReryAsync(AwsCognitoUser user);
+        Task<HttpResponseMessage> LoginWithRetryAsync(AwsCognitoUser user);
+        Task<HttpResponseMessage> ResetPasswordWithRetryAsync(AwsCognitoUser user);
+        Task<HttpResponseMessage> ConfirmPasswordWithRetryAsync(AwsCognitoUser user);
+    }
+}
